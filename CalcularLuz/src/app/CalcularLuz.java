ed7da6080e1bc3aa1cfcbcab67f024c5bcb43742
Yber-M <ybermarlon@gmail.com>
2023-06-29 12:03:18 -0500

    Upload

---
+++ CalcularLuz/src/app/CalcularLuz.java
@@ -0,0 +1,16 @@
+/**
+ * @author Yber-M;
+ * @email ybermarlon@gmail.com
+ * @github https://github.com/Yber-M
+ * @facebook https://www.facebook.com/MarlonYber.QO
+ */
+
+package app;
+
+public class CalcularLuz {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
